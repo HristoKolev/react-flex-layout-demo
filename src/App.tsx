8ec3fa675e43699bec6befde5fc74e6038bd21a9
Hristo Kolev <hristo.a.kolev@gmail.com>
2023-04-11 23:50:21 +0300

    tab demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { Layout, Model, TabNode, IJsonModel } from 'flexlayout-react';
+import { memo, useRef } from 'react';
+import { IJsonModel, Layout, Model, TabNode } from 'flexlayout-react';
 
-import 'flexlayout-react/style/light.css';
+import 'flexlayout-react/style/dark.css';
 
-const json: IJsonModel = {
+const RenderCounter = (): JSX.Element => {
+  const countRef = useRef(0);
+  countRef.current += 1;
+  return <span>Render Count - {countRef.current}</span>;
+};
+
+const App1 = memo(
+  (): JSX.Element => (
+    <div>
+      App 1 | <RenderCounter />
+    </div>
+  )
+);
+
+const App2 = memo(
+  (): JSX.Element => (
+    <div>
+      App 2 | <RenderCounter />
+    </div>
+  )
+);
+
+const App3 = memo(
+  (): JSX.Element => (
+    <div>
+      App 3 | <RenderCounter />
+    </div>
+  )
+);
+
+const App4 = memo(
+  (): JSX.Element => (
+    <div>
+      {' '}
+      App 4 | <RenderCounter />{' '}
+    </div>
+  )
+);
+
+const App5 = memo(
+  (): JSX.Element => (
+    <div>
+      App 5 | <RenderCounter />
+    </div>
+  )
+);
+
+const modelJson: IJsonModel = {
   global: {
     tabEnableFloat: false,
+    tabEnableClose: false,
   },
   borders: [],
   layout: {
     type: 'row',
-    weight: 100,
+    weight: 100, // ???
     children: [
       {
         type: 'tabset',
-        weight: 50,
+        weight: 50, // ???
         children: [
           {
             type: 'tab',
-            name: 'One',
-            component: 'button',
+            name: 'App 1',
+            component: 'app-1',
           },
-        ],
-      },
-      {
-        type: 'tabset',
-        weight: 50,
-        children: [
           {
             type: 'tab',
-            name: 'Two',
-            component: 'button',
+            name: 'App 2',
+            component: 'app-2',
+          },
+          {
+            type: 'tab',
+            name: 'App 3',
+            component: 'app-3',
+          },
+          {
+            type: 'tab',
+            name: 'App 4',
+            component: 'app-4',
+          },
+          {
+            type: 'tab',
+            name: 'App 5',
+            component: 'app-5',
           },
         ],
       },
@@ -37,18 +95,28 @@ const json: IJsonModel = {
   },
 };
 
-const model = Model.fromJson(json);
+const model = Model.fromJson(modelJson);
 
-export const App = (): JSX.Element => {
-  const factory = (node: TabNode) => {
-    console.log('factory called');
-
-    const component = node.getComponent();
-
-    if (component === 'button') {
-      return <button>{node.getName()}</button>;
+const createComponent = (node: TabNode) => {
+  switch (node.getComponent()) {
+    case 'app-1': {
+      return <App1 />;
     }
-  };
-
-  return <Layout model={model} factory={factory} />;
+    case 'app-2': {
+      return <App2 />;
+    }
+    case 'app-3': {
+      return <App3 />;
+    }
+    case 'app-4': {
+      return <App4 />;
+    }
+    case 'app-5': {
+      return <App5 />;
+    }
+  }
 };
+
+export const App = (): JSX.Element => (
+  <Layout model={model} factory={createComponent} />
+);
