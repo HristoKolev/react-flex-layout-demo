0a3b63f82f35365667eebb220c9891f3e606220c
Hristo Kolev <hristo.a.kolev@gmail.com>
2023-04-11 23:12:51 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Model, TabNode, IJsonModel } from 'flexlayout-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'flexlayout-react/style/light.css';
 
-export default App;
+const json: IJsonModel = {
+  global: {
+    tabEnableFloat: false,
+  },
+  borders: [],
+  layout: {
+    type: 'row',
+    weight: 100,
+    children: [
+      {
+        type: 'tabset',
+        weight: 50,
+        children: [
+          {
+            type: 'tab',
+            name: 'One',
+            component: 'button',
+          },
+        ],
+      },
+      {
+        type: 'tabset',
+        weight: 50,
+        children: [
+          {
+            type: 'tab',
+            name: 'Two',
+            component: 'button',
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const model = Model.fromJson(json);
+
+export const App = (): JSX.Element => {
+  const factory = (node: TabNode) => {
+    console.log('factory called');
+
+    const component = node.getComponent();
+
+    if (component === 'button') {
+      return <button>{node.getName()}</button>;
+    }
+  };
+
+  return <Layout model={model} factory={factory} />;
+};
